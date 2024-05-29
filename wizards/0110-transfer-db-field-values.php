ee44e082367cb259015d3bada06f24a50f68cb92
Christian Pschorr <pschorr.christian@gmail.com>
2024-05-29 10:35:20 +0200

    Migration der Tabellendefinition von tx_contact_domain_model_location

--- wizards/0110-transfer-db-field-values.php
+++ wizards/0110-transfer-db-field-values.php
@@ -58,6 +58,16 @@ $query = 'UPDATE sys_category SET
 	WHERE uid IN (195)';
 $result = $db->query($query);
 
+$query = 'UPDATE sys_category SET 
+		tx_foundation_identifier = "contact-countries"
+	WHERE uid IN (12)';
+$result = $db->query($query);
+
+$query = 'UPDATE sys_category SET 
+		tx_foundation_identifier = "contact-product-lines"
+	WHERE uid IN (202)';
+$result = $db->query($query);
+
 // Address
 $query = 'UPDATE tt_address SET 
 	tx_foundation_directors = tx_xo_directors,
